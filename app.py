9217760202e8adb60199335572dfdb389cdd1fee
gibranrafii <gibranrafi21@gmail.com>
2024-12-24 23:23:15 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, redirect, url_for, request, render_template 
+app = Flask(__name__)
+
+@app.route('/success/<name>')
+def success(name):
+   return render_template('success.html', name=name)
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   return render_template('login.html')  
+
+if __name__ == '__main__':
+   app.run(debug = True)
